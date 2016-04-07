b111a36efbd5640ccd2baa4951e49171030a2ce7
Chris Norwood <cnorwood92@gmail.com>
2016-04-07 16:57:51 -0700

    Wrote helper functions for DOM interaction, changed text input to resize prompt button.  All basic functionality exists.

--- js/application.js
+++ js/application.js
@@ -1,22 +1,55 @@
 $(document).ready(function(){
+	drawGrid(16);
+	sketch();
 	
-	var dimension = 16;
+	// Resize Button
+	$('button').filter('.resize').on('click', function(){
+		var dim = prompt('How many boxes wide for the grid?');
 
+		removeGrid();
+		drawGrid(dim);
+
+		$('.square').css({height: (480/dim)+'px', width: (480/dim)+'px'});
+
+		sketch();
+	});
+
+	// Reset Button
+	$('button').filter('.reset').on('click', function() {
+		eraseGrid();
+	});
+});
+
+// Helper Functions
+
+var drawGrid = function (dim) {
 	var $row = $('<div />', {class: 'row'})
-	var $square = $('<div />', {class: 'square'});
+	var $square_init = $('<div />', {class: 'square'});
 	
-	// This fills a row with squares, amount according to dimension variable
-	for(var i = 0; i < dimension; i++) {
-		$row.append($square.clone());
+	// This fills a row with squares, amount according to gridWidth variable
+	for(var i = 0; i < dim; i++) {
+		$row.append($square_init.clone());
 	}
 
-	// This fills the grid with rows, amount according to dimension variable
-	for(var i = 0; i < dimension; i++) {
+	// This fills the grid with rows, amount according to gridWidth variable
+	for(var i = 0; i < dim; i++) {
 		$('#grid').append($row.clone());
 	}
+};
 
+var removeGrid = function () {
+	$('.square').remove();
+	$('.row').remove();
+};
+
+var sketch = function () {
 	$('.square').on('mouseenter', function() {
 		$(this).css('background', '#000');
 	});
+};
 
-});
+var eraseGrid = function () {
+	$('.square').css('background', '#fff');
+};
+
+
