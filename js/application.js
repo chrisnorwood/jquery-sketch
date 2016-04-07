ae8bd69261a3c90f811f3ec0d9b283d73e7fc0fb
Chris Norwood <cnorwood92@gmail.com>
2016-04-07 15:18:05 -0700

    Basic static grid creation and etch-a-sketch functionality

---
+++ js/application.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+	var dimension = 16;
+
+	var $row = $('<div />', {class: 'row'})
+	var $square = $('<div />', {class: 'square'});
+	
+	// This fills a row with squares, amount according to dimension variable
+	for(var i = 0; i < dimension; i++) {
+		$row.append($square.clone());
+	}
+
+	// This fills the grid with rows, amount according to dimension variable
+	for(var i = 0; i < dimension; i++) {
+		$('#grid').append($row.clone());
+	}
+
+	$('.square').on('mouseover', function() {
+		$(this).css('background', '#000');
+	});
+
+});
